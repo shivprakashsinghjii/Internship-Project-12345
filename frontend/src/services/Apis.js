fbb0166c59c063fef9fe301964f1c9be8aad7813
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2024-07-25 16:44:37 +0530

    aaaaaaaaaaa

--- frontend/src/services/Apis.js
+++ frontend/src/services/Apis.js
@@ -2,13 +2,13 @@ import { commonrequest } from "./ApiCall";
 import { BACKEND_URL } from "./helper";
 
 export const registerfunction = async (data) => {
-  return await commonrequest("POST", `${BACKEND_URL}/user/register`, data);
+  return await commonrequest("POST", `${BACKEND_URL}`, data);
 };
 
 export const sentOtpFunction = async (data) => {
-  return await commonrequest("POST", `${BACKEND_URL}/user/sendotp`, data);
+  return await commonrequest("POST", `${BACKEND_URL}`, data);
 };
 
 export const userVerify = async (data) => {
-  return await commonrequest("POST", `${BACKEND_URL}/user/login`, data);
+  return await commonrequest("POST", `${BACKEND_URL}`, data);
 };
